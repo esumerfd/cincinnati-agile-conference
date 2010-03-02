f20ebef3db0a509245714e5b0ac59b855db42f10
Ed Sumerfield <esumerfd@bitbashers.org>
2010-03-02 11:42:42 -0500

    Allow the deploy to proxy the ssh authentication through the deployer.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 default_run_options[:pty] = true
 
+set :ssh_options, {:forward_agent => true}
+
 # be sure to change these
 set :user, 'cincyagilecon'
 set :domain, 'bitbashers.org'
@@ -7,7 +9,7 @@ set :application, 'cincydayofagile.org'
 
 # the rest should be good
 set :repository,  "git@github.com:esumerfd/cincinnati-agile-conference.git"
-set :deploy_to, "/home/#{user}/#{application}x"
+set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
 set :scm, 'git'
 set :branch, 'master'
